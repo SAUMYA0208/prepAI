385558e0288ef13160fcc3285bac3655e892bdeb
SAUMYA0208 <shuklasaumya203@gmail.com>
2026-04-12 19:14:09 +0530

    Added env check for API key

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config()   // must be first line
+console.log("ENV CHECK:", process.env.GEMINI_API_KEY);
 
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
