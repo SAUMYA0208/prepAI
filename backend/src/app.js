0a4d93f68dd02148656acb692bc83e48ed83b18c
SAUMYA0208 <shuklasaumya203@gmail.com>
2026-04-12 21:23:45 +0530

    fix cors order for cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,14 @@ const cors = require("cors")
 
 const app = express()
 
-app.use(express.json())
-app.use(cookieParser())
 app.use(cors({
      origin: "https://prep-ai-taupe.vercel.app",
   credentials: true
 }))
+
+app.use(express.json())
+app.use(cookieParser())
+
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
